1382bdd3e406ec4a2af8a4806b01dc3bd06d26d2
duoduo5566 <347032779@qq.com>
2015-08-13 00:02:33 +0800

    add code for ExcelInputFormat...

--- src/org/czs/hadoop/excel/ExcelInputFormat.java
+++ src/org/czs/hadoop/excel/ExcelInputFormat.java
@@ -1,19 +1,24 @@
 package org.czs.hadoop.excel;
 
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.util.LineReader;
 
 
-public class ExcelInputFormat extends FileInputFormat< Text, IntWritable> {
+public class ExcelInputFormat extends FileInputFormat< LongWritable, Text> {
 
 	@Override
 	protected boolean isSplitable(JobContext context, Path filename) {
@@ -22,38 +27,65 @@ public class ExcelInputFormat extends FileInputFormat< Text, IntWritable> {
 	}
 
 	@Override
-	public RecordReader<Text, IntWritable> createRecordReader(InputSplit split, TaskAttemptContext context)
+	public RecordReader<LongWritable, Text> createRecordReader(InputSplit split, TaskAttemptContext context)
 			throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		return new ExcelRecordReader();
 	}
 	
-	public static class ExcelRecordReader extends RecordReader<Text, IntWritable>{
-		public LineReader in; //行读取器
-		public Text key; // 自定义key类型
-		public IntWritable value; // 自定义value类型
-		public Text line; // 每行数据类型
+	public static class ExcelRecordReader extends RecordReader<LongWritable, Text>{
+		public InputStream is; //行读取器
+		public String[] strArrayofLines; // 每行数据类型
+		public LongWritable key; // 自定义key类型
+		public Text value; // 自定义value类型
 
 		@Override
-		public void initialize(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
+		public void initialize(InputSplit input, TaskAttemptContext context) throws IOException, InterruptedException {
 			// TODO Auto-generated method stub
+			FileSplit split = (FileSplit) input;
+			Configuration conf = context.getConfiguration();
+			Path file = split.getPath();
+			FileSystem fs = file.getFileSystem(conf);
 			
+			FSDataInputStream fileIn = fs.open(file);
+			
+			is = fileIn;
+			String line = new ExcelParser().parseExcelData(is);
+			this.strArrayofLines = line.split("\n");
 		}
 
 		@Override
 		public boolean nextKeyValue() throws IOException, InterruptedException {
 			// TODO Auto-generated method stub
-			return false;
+			if (key == null){
+				key = new LongWritable(0);
+				value = new Text(strArrayofLines[0]);
+			} else {
+				if (key.get() < this.strArrayofLines.length - 1){
+					long pos =(int) key.get();
+					
+					key.set(pos + 1);
+					value.set(strArrayofLines[(int) (pos + 1)]);
+				} else {
+					return false;
+				}
+			}
+			
+			if (key == null || value == null){
+				return false;
+			} else {
+				return true;
+			}
 		}
 
 		@Override
-		public Text getCurrentKey() throws IOException, InterruptedException {
+		public LongWritable getCurrentKey() throws IOException, InterruptedException {
 			// TODO Auto-generated method stub
 			return key;
 		}
 
 		@Override
-		public IntWritable getCurrentValue() throws IOException, InterruptedException {
+		public Text getCurrentValue() throws IOException, InterruptedException {
 			// TODO Auto-generated method stub
 			return value;
 		}
@@ -67,8 +99,8 @@ public class ExcelInputFormat extends FileInputFormat< Text, IntWritable> {
 		@Override
 		public void close() throws IOException {
 			// TODO Auto-generated method stub
-			if (in != null){
-				in.close();
+			if (is != null){
+				is.close();
 			}
 			
 		}
